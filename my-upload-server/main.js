bb309151eaed1e10a5f52325cb8b774cca8b7af0
Roy <Roy@123.com>
2024-11-11 15:33:18 +0800

    rep.body

--- my-upload-server/main.js
+++ my-upload-server/main.js
@@ -3,10 +3,12 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(cors());
+app.use(express.json());
 
 app.post('/hello', (req, res) => {
     res.send('Hello World!')
     console.log('请求路径:', req.path); // 打印请求路径
+    console.log('请求参数:', req.body); // 打印请求参数
 })
 
 app.listen(port,'',() => {
